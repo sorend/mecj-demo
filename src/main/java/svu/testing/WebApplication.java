163029b13bdfdc47faa9a0546d062695a210c283
Soren Atmakuri Davidsen <soren@atmakuridavidsen.com>
2015-03-25 10:48:34 +0530

    removed webbit

--- src/main/java/svu/testing/WebApplication.java
+++ src/main/java/svu/testing/WebApplication.java
@@ -1,31 +1,53 @@
 package svu.testing;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.Reader;
-import java.net.URL;
-
-import org.webbitserver.HttpControl;
-import org.webbitserver.HttpHandler;
-import org.webbitserver.HttpRequest;
-import org.webbitserver.HttpResponse;
-import org.webbitserver.WebServer;
-import org.webbitserver.WebServers;
-import org.webbitserver.handler.StaticFileHandler;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import svu.meclassifier.MinkowskiDistanceFunction;
 import svu.meclassifier.MultimodalEvolutionaryClassifier;
 import svu.util.CSVUtil;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class WebApplication {
 
 	private static MultimodalEvolutionaryClassifier mec = null;
 	
-	private static double param(HttpRequest req, String key) {
-		return Double.parseDouble(req.queryParam(key));
+	private static double param(Map<String, String> req, String key) {
+		return Double.parseDouble(req.get(key));
 	}
+
+	public static Map<String, String> queryToMap(String query){
+	    Map<String, String> result = new HashMap<String, String>();
+	    if (query == null)
+	    	return result;
+	    
+	    for (String param : query.split("&")) {
+	        String pair[] = param.split("=");
+	        if (pair.length>1) {
+	            result.put(pair[0], pair[1]);
+	        }else{
+	            result.put(pair[0], "");
+	        }
+	    }
+	    return result;
+	}		
 	
-	private static double[] params(HttpRequest req) {
+
+	private static double[] params(HttpExchange t) {
+		
+		Map<String, String> req = queryToMap(t.getRequestURI().getQuery());
+		
 		return new double[]{ 
 				param(req, "preg"),
 				param(req, "plas"),
@@ -37,41 +59,85 @@ public class WebApplication {
 				param(req, "age")
 		};
 	}
+
+	public static void sendResponse(HttpExchange t, int code, String message, String contentType) throws IOException {
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		b.write(message.getBytes());
+		sendResponse(t,  code,  b,  contentType);
+	}
+
+	public static void sendResponse(HttpExchange t, int code, ByteArrayOutputStream message, String contentType) throws IOException {
+		long length = message.size();
+		if (contentType != null) t.getResponseHeaders().add("Content-Type", contentType);
+		t.sendResponseHeaders(code, length);
+		OutputStream os = t.getResponseBody();
+		os.write(message.toByteArray());
+		os.close();
+	}
+
+	public static class StaticContentHandler implements HttpHandler {
+		private File path;
+		private String prefix;
+		public StaticContentHandler(String prefix, File path) {
+			this.prefix = prefix;
+			this.path = path;
+		}
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			String path = t.getRequestURI().getPath().substring(prefix.length());
+			File get = new File(this.path, path);
+			if (!get.exists())
+				sendResponse(t, 404, path, "text/plain");
+			else {
+				FileInputStream fr = new FileInputStream(get);
+				ByteArrayOutputStream b = new ByteArrayOutputStream();
+				byte[] buf = new byte[4096];
+				while (true) {
+					int read = fr.read(buf);
+					if (read == -1)
+						break;
+					b.write(buf, 0, read);
+				}
+				fr.close();
+				sendResponse(t, 200, b, null);
+			}
+		}
+	}
 	
 	public static class PredictionHandler implements HttpHandler {
+		
 		@Override
-		public void handleHttpRequest(HttpRequest req, HttpResponse res, HttpControl ctrl) throws Exception {
+		public void handle(HttpExchange t) throws IOException {
 			if (mec == null) {
-				res.status(500).end();
+				sendResponse(t, 500, "Classifier not ready", "text/plain");
 			}
 			else {
-				double[] newUserParams = params(req);
+				double[] newUserParams = params(t);
 				int[] prediction = mec.predict(new double[][]{ newUserParams });
 				// send reply to client
-				res.header("Content-Type", "application/json")
-					.content("{\"result\":" + prediction[0] + "}")
-					.end();
+				sendResponse(t, 200, "{\"result\":" + prediction[0] + "}", "application/json");
 			}
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
+
+		String userDir = System.getProperty("user.dir");
+		File path = new File(new File(new File(userDir, "src"), "main"), "public");
+		System.out.println("path = " + path);
 		
-		URL diabetesLocation = WebApplication.class.getResource("/svu/testing/diabetes.csv");
-		File path = new File(new File(diabetesLocation.toURI()).getParentFile().getParentFile().getParentFile().getParentFile(), "web");
+		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+		server.createContext("/public", new StaticContentHandler("/public", path));
+		server.createContext("/predict", new PredictionHandler());
+		server.start();
 		
-		WebServer server = WebServers.createWebServer(8080)
-			.add("/predict", new PredictionHandler())
-			.add(new StaticFileHandler(path))
-			.start().get();
-		
-		System.out.println("Web-server started, please open " + server.getUri() + "index.html");
+		System.out.println("Web-server started, please open http://127.0.0.1:8080/public/index.html");
 		
 		MultimodalEvolutionaryClassifier classifier =
 				new MultimodalEvolutionaryClassifier(100, MinkowskiDistanceFunction.Factory(3.0));
 		
 		// read dataset
-		Reader reader = new InputStreamReader(MECMain.class.getResourceAsStream("diabetes.csv"));
+		Reader reader = new InputStreamReader(WebApplication.class.getResourceAsStream("/diabetes.csv"));
 		double[][] data = CSVUtil.load(reader);
 		double[][] X = CSVUtil.attributes(data);
 		int[]      Y = CSVUtil.classes(data);
